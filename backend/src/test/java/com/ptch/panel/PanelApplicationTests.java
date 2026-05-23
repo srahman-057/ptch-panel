dc618b105c25689f86356d5cf77e69842ccdf430
srahman-057 <13519776+srahman-057@users.noreply.github.com>
2026-05-22 23:41:00 -0400

    Backend init

---
+++ backend/src/test/java/com/ptch/panel/PanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptch.panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
